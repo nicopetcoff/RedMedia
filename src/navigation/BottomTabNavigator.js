e1724c1c625ff2af69a6481b46991838cb7e1860
Nicolas <nicolaspetcoff@gmail.com>
2024-10-11 16:54:04 -0300

    correcion navbar

--- src/navigation/BottomTabNavigator.js
+++ src/navigation/BottomTabNavigator.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/Ionicons';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeIcon from '../assets/home.svg'; // Importa tu archivo SVG
+import SearchIcon from '../assets/search.svg';
+import CreatePostIcon from '../assets/add_circle.svg';
+import NotificationIcon from '../assets/heart.svg';
+import ProfileIcon from '../assets/profile.svg';
 import HomeStackScreen from './HomeStackScreen'; // Importamos HomeStackScreen
 import SearchScreen from '../screens/SearchScreen';
 import CreatePostScreen from '../screens/CreatePostScreen';
@@ -9,35 +13,34 @@ import ProfileScreen from '../screens/ProfileScreen';
 
 const Tab = createBottomTabNavigator();
 
-const TabBarIcon = ({ route, color, size }) => {
-  let iconName;
-
-  if (route.name === 'HomeStack') {
-    iconName = 'home-outline';
-  } else if (route.name === 'Search') {
-    iconName = 'search-outline';
-  } else if (route.name === 'CreatePost') {
-    iconName = 'add-circle-outline';
-  } else if (route.name === 'Notifications') {
-    iconName = 'heart-outline';
-  } else if (route.name === 'Profile') {
-    iconName = 'person-outline';
+const TabBarIcon = ({route, color, size}) => {
+  // Usamos diferentes íconos dependiendo de la ruta
+  switch (route.name) {
+    case 'HomeStack':
+      return <HomeIcon width={size} height={size} />;
+    case 'Search':
+      return <SearchIcon width={size} height={size} />;
+    case 'CreatePost':
+      return <CreatePostIcon width={size} height={size} />;
+    case 'Notifications':
+      return <NotificationIcon width={size} height={size} />;
+    case 'Profile':
+      return <ProfileIcon width={size} height={size} />;
+    default:
+      return null;
   }
-
-  return <Icon name={iconName} size={size} color={color} />;
 };
 
 const BottomTabNavigator = () => {
   return (
     <Tab.Navigator
-      screenOptions={({ route }) => ({
-        headerShown: false, 
-        tabBarIcon: (props) => <TabBarIcon route={route} {...props} />,
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarIcon: props => <TabBarIcon route={route} {...props} />,
         tabBarActiveTintColor: 'black',
         tabBarInactiveTintColor: 'gray',
-        tabBarShowLabel: false, 
-      })}
-    >
+        tabBarShowLabel: false,
+      })}>
       <Tab.Screen name="HomeStack" component={HomeStackScreen} />
       <Tab.Screen name="Search" component={SearchScreen} />
       <Tab.Screen name="CreatePost" component={CreatePostScreen} />
