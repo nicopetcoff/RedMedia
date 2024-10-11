d12f5682fd2651efac89d6a12b513705fa377dfa
Nicolas <nicolaspetcoff@gmail.com>
2024-10-11 17:03:24 -0300

    post detail corregido

--- src/navigation/HomeStackScreen.js
+++ src/navigation/HomeStackScreen.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { TouchableOpacity } from 'react-native';
 import HomeScreen from '../screens/HomeScreen';
 import PostDetail from '../screens/PostDetail';
+import BackIcon from '../assets/back.svg'; // Icono personalizado de regreso
 
 const Stack = createStackNavigator();
 
-const HomeStackScreen = () => {
+const HomeStackScreen = ({ navigation }) => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-      <Stack.Screen name="PostDetail" component={PostDetail} options={{ headerShown: true }} />
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="PostDetail"
+        component={PostDetail}
+        options={{
+          headerTitle: '', // Ocultamos el título
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => navigation.goBack()}
+              style={{ marginLeft: 10 }}>
+              <BackIcon width={24} height={24} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
     </Stack.Navigator>
   );
 };
