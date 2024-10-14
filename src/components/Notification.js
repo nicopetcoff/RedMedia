afa90be1f353f48b14babc392b5d2a532d44f10f
Mauricio Escalante <escalantemauricio2002@gmail.com>
2024-10-13 23:30:59 -0300

    Notificaciones

---
+++ src/components/Notification.js
@@ -0,0 +1,79 @@
+// src/components/Notification.js
+import React from 'react';
+import {View, Text, StyleSheet,Image} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+
+const Notification = ({item}) => {
+  const navigation = useNavigation();
+
+  const getImage=()=> {
+    switch (item.type) {
+      case 'Trending':
+        return require('../assets/like.png');
+      case 'Comment':
+        return require('../assets/comment.png');
+      case 'Followed':
+        return require('../assets/followed.png');
+      default:
+        return require('../assets/comment.png');
+    }
+  }
+
+  return (
+    <View style={styles.Notification}>
+        <Image source={getImage()} style={styles.imagen} />
+        <View style={styles.activityItem}>
+            <Text style={styles.activityType}>{item.type}</Text>
+            <View style={styles.action}>
+                {item.icon}
+                <Text style={styles.activityUser}>{item.user}</Text>
+                <Text style={styles.activityText}>{item.text}</Text>
+            </View>
+            <Text style={styles.activityTime}>{item.time}</Text>
+        </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    Notification: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+  activityItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(242, 244, 245, 0.5)',
+    flex: 1,
+  },
+  activityType: {
+    fontWeight: 'bold',
+    lineHeight: 22,
+    fontSize: 17,
+    color: 'black',
+    marginRight: 5,
+  },
+  action: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  activityUser: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    fontFamily: 'Roboto',
+    color: 'black',
+  },
+  activityText: {
+    marginLeft: 8,
+    fontSize: 16,
+    color: 'black',
+  },
+  activityTime: {
+    marginTop: 5,
+    alignSelf: 'flex-end',
+    color: '#999',
+    fontSize: 12,
+  },
+});
+
+export default Notification;
--- src/screens/NotificationScreen.js
+++ src/screens/NotificationScreen.js
@@ -1,11 +1,21 @@
 // src/screens/NotificationScreen.js
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {View, Text,FlatList, StyleSheet} from 'react-native';
+import Notification from '../components/Notification';
+import notificaciones from '../data/notificaciones.json';
 
 const NotificationScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Notification Screen</Text>
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>Activity</Text>
+      </View>
+      <FlatList
+        data={notificaciones}
+        renderItem={({item}) => <Notification item={item} />}
+        keyExtractor={item => item.id}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+      />
     </View>
   );
 };
@@ -13,8 +23,23 @@ const NotificationScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: '#fff',
+    paddingHorizontal: 10,
+  },
+  headerContainer: {
+    paddingVertical: 30,
+  },
+  header: {
+    fontSize: 26,
+    fontFamily: 'Roboto',
+    fontWeight: 'bold',
+    color: 'black',
+    alignSelf: 'center',
+    marginTop: 10,
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#ddd',
   },
 });
 
